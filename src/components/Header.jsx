aaf66ac27c169f9c43c6694a7c7e702dc23631ae
KeremZeybek <recepkeremzeybek@gmail.com>
2023-07-18 11:46:09 +0300

    add todo function added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,26 @@
+import { useState } from 'react';
 import { Button } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 
 const Header = () => {
+    const [task,setTast]=useState("")
+    const handleClick=()=>{
+        console.log(task)
+    }
   return (
     <div>
    <h1 className="text-center text-danger m-5">Todo App</h1>
-   <InputGroup className="mb-3">
+   <InputGroup className="mb-3 w-50 d-flex mx-auto">
         <Form.Control
+        className='rounded-3'
           placeholder="Enter new Todo..."
           aria-label="Recipient's username"
           aria-describedby="basic-addon2"
+          value={task}
+          onChange={(e)=>(setTast(e.target.value))}
         />
-        <Button className="input-group-text bg-success" id="basic-addon2">Add Todo</Button>
+        <Button className="input-group-text bg-success rounded-2 mx-2" onClick={handleClick} id="basic-addon2">Add Todo</Button>
       </InputGroup>
     </div>
   )
