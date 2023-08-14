6731ea860ef481b97d74419da81bcb6a18ee65c5
KeremZeybek <recepkeremzeybek@gmail.com>
2023-08-14 16:13:34 +0300

    delete svg and delete func added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import "./App.css";
 import Header from "./components/Header";
 import TodoList from "./components/TodoList";
 
+
+const todoList =[
+  {
+    id:new Date().getTime(),
+    text:"React ile TodoList uygulaması",
+    completed:false,
+  },
+  {
+    id:new Date().getTime(),
+    text:"JavaScript çalışmalısın",
+    completed:false,
+  }
+]
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(todoList);
   return (
     <div className="container">
       <Header setTodos={setTodos} todos={todos} />
-      <TodoList todos={todos} />
+      <TodoList todos={todos} setTodos={setTodos}/>
     </div>
   );
 }
